4306a98a0e53e66d9e9e4fcaac58cd9e281d8552
Kesrn <526983659@qq.com>
2019-04-09 14:51:41 +0800

    【测试2】

--- src/main/java/com/zc/partymember/common/ApiConst.java
+++ src/main/java/com/zc/partymember/common/ApiConst.java
@@ -21,25 +21,10 @@ public interface ApiConst {
     String API_USER_DELETE = "/{id}/delete";
     String API_USER_PWD_CHANGE = "/{id}/changePassword";
 
-    String API_QUESTION = "/question";
-    String API_QUESTION_CREATE = "/create";
-    String API_QUESTION_TEMPORARY = "/temporary";
-    String API_QUESTION_DELETE = "/{id}/delete";
-    String API_QUESTION_CHANGESTATUS = "/{id}/changeStatus";
-    String API_QUESTION_CHANGETYPE = "/{id}/changeType";
-
-    String API_QUESTIONNAIRE = "/partymember";
-    String API_QUESTIONNAIRE_CREATE ="/create";
-    String API_QUESTIONNAIRE_DELETE ="/{id}/delete";
-    String API_QUESTIONNAIRE_CHANGEANSWER ="/{id}/changeAnswer";
-
     String API_DEPT = "/dept";
     String API_DEPT_CREATE = "/create";
     String API_DEPT_UPDATE = "/{id}/update";
     String API_DEPT_DELETE = "/{id}/delete";
-
-    String API_QUESTION_JOINS = "/questionJoins";
-    String API_QUESTION_JOINS_LIST = "/list";
 
     String API_MEMBER = "/member";
     String API_MEMBER_CREATE = "/create";
